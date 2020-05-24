518a514b750593d62fb7b247937936413d2bbd04
Daniel Stoll <danielsimon.stoll2@gmail.com>
2020-05-24 13:01:26 +0200

    Improved EventDataReader#readData()

--- kafka-producer/src/main/kotlin/de/dstoll/kafkaproducer/service/EventDataReader.kt
+++ kafka-producer/src/main/kotlin/de/dstoll/kafkaproducer/service/EventDataReader.kt
@@ -1,17 +1,24 @@
 package de.dstoll.kafkaproducer.service
 
 import de.dstoll.kafkaproducer.properties.EventDataProperties
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
 import java.io.FileReader
 import java.io.IOException
 import java.lang.RuntimeException
 import java.nio.file.Files
+import kotlin.system.exitProcess
 
 @Service
-class EventDataReader(private val eventDataProperties: EventDataProperties) {
+class EventDataReader(
+        private val eventDataProperties: EventDataProperties,
+        private val applicationContext: ApplicationContext
+) {
 
-    private var reader: BufferedReader = BufferedReader(FileReader(eventDataProperties.location))
+    private val reader: BufferedReader = BufferedReader(FileReader(eventDataProperties.location))
 
     fun readData(): String {
         return try {
@@ -20,10 +27,10 @@ class EventDataReader(private val eventDataProperties: EventDataProperties) {
                 line
             } else {
                 reader.close()
-                reader = BufferedReader(FileReader(eventDataProperties.location))
-                readData()
+                exitProcess(SpringApplication.exit(applicationContext))
             }
         } catch (iox: IOException) {
+            reader.close()
             throw RuntimeException("Fehler beim Einlesen der Event-Daten!")
         }
     }
