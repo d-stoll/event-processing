e751415832c6fe8947d8a43c52d114b26ef83b86
Daniel Stoll <danielsimon.stoll2@gmail.com>
2020-07-11 17:04:59 +0200

    Bugfix

--- ep-kafka/kafka.sh
+++ ep-kafka/kafka.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(dirname "$0")
 
 if [ ! -d "$BASEDIR/bin" ]; then
-    unzip kafka.zip
+    unzip $BASEDIR/kafka.zip -d $BASEDIR
 fi
 
 $BASEDIR/bin/kafka-server-start.sh $BASEDIR/config/server.properties
--- ep-kafka/zookeeper.sh
+++ ep-kafka/zookeeper.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(dirname "$0")
 
 if [ ! -d "$BASEDIR/bin" ]; then
-    unzip kafka.zip
+    unzip $BASEDIR/kafka.zip -d $BASEDIR
 fi
 
 $BASEDIR/bin/zookeeper-server-start.sh $BASEDIR/config/zookeeper.properties
